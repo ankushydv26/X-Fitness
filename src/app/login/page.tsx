9763f3a245bc1929a2da64f7ac6661a0a15a2045
ankush kumar <ankush@gmail.com>
2024-04-02 16:20:51 +0530

    added front end side code

---
+++ src/app/login/page.tsx
@@ -0,0 +1,73 @@
+"use client"
+import React, { useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { json } from 'stream/consumers';
+
+const LoginPage = () => {
+  const router = useRouter();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e: { preventDefault: () => void; }) => {
+    e.preventDefault();
+    // Perform login logic here, e.g., call an API to authenticate the user
+    console.log('Email:', email);
+    console.log('Password:', password);
+    // If authentication is successful, redirect to the dashboard or home page
+    
+  };
+
+  return (
+    <div className="flex justify-center items-center h-screen px-4">
+      <div className="bg-white p-8 rounded-lg shadow-md max-w-md w-full">
+        <h2 className="text-2xl font-bold mb-6 text-center">Login</h2>
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <label htmlFor="email" className="block text-gray-700 font-bold mb-2">
+              Email
+            </label>
+            <input
+              type="email"
+              id="email"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your email"
+              required
+            />
+          </div>
+          <div className="mb-6">
+            <label htmlFor="password" className="block text-gray-700 font-bold mb-2">
+              Password
+            </label>
+            <input
+              type="password"
+              id="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+              placeholder="Enter your password"
+              required
+            />
+          </div>
+          <div className="flex flex-col items-center sm:flex-row sm:justify-between">
+            <button
+              type="submit"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-4 sm:mb-0"
+            >
+              Login
+            </button>
+            <a
+              href="#"
+              className="inline-block align-baseline font-bold text-sm text-blue-500 hover:text-blue-800"
+            >
+              Forgot Password?
+            </a>
+          </div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default LoginPage;
